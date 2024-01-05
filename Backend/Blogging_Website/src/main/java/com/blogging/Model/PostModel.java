cc21295bf8a7964f8aaac3c9a385fc0221d77160
Mirza Tabish Hasan <73854161+Tabish8065@users.noreply.github.com>
2024-01-05 10:07:26 +0000

    Post Model Schema Documented

--- Backend/Blogging_Website/src/main/java/com/blogging/Model/PostModel.java
+++ Backend/Blogging_Website/src/main/java/com/blogging/Model/PostModel.java
@@ -3,6 +3,7 @@ package com.blogging.Model;
 import java.util.HashSet;
 import java.util.Set;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
 import jakarta.persistence.FetchType;
@@ -18,16 +19,36 @@ import lombok.*;
 @AllArgsConstructor
 @NoArgsConstructor
 @Entity
+@Schema(
+    description = "The is the Post Model for the database"
+)
 public class PostModel {
     
 	@Id
 	@GeneratedValue(strategy = GenerationType.AUTO)
+    @Schema(
+        description = "The primary key for the post in the database. The key is auto-generated."
+    )
     private int id;
     
+    @Schema(
+        description = "The title for the post."
+    )
     private String title;
+
+    @Schema(
+        description = "Description for the post."
+    )
     private String description;
+    
+    @Schema(
+        description = "The content of the post"
+    )
     private String content;
 
+    @Schema(
+        description = "The comment associated wwith the post. Comment should be added using comment API after post has been created in the database."
+    )
     @OneToMany(mappedBy = "post", cascade = CascadeType.ALL, orphanRemoval = true)
     private Set<CommentModel> comments = new HashSet<CommentModel>();
 
